796e4039f7191a407cc719c56dc5c87407fed4b2
nickrmeier <nickrmeier@gmaill.com>
2020-12-15 14:49:16 -0800

    restAPI fully functional

--- code/app.py
+++ code/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+from secret import key
+
 
 app = Flask(__name__)
 api = Api(app)
+app.secret_key = key
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x["name"] == name, items), None)
         return {"item": item}, 200 if item else 404
@@ -20,6 +27,27 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help="This field cannot be left blank!"
+        )
+        data = parser.parse_args()
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        if item is None:
+            item = {"name": name, "price": data["price"]}
+            items.append(item)
+            return item, 201
+        else:
+            item.update(data)
+            return item, 201
 
 class ItemsList(Resource):
     def get(self):

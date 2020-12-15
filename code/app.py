f7a15f7c0c5420de90e126dd6c7a965c61095462
nickrmeier <nickrmeier@gmaill.com>
2020-12-14 17:39:47 -0800

    REST API tested in postman

---
+++ code/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        return {"item": item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x["name"] == name, items), None) is not None:
+            return {'message': "an item with name '{}' already exists.".format(name)}, 404
+        data = request.get_json()
+        item = {"name": name, "price": data["price"]}
+        items.append(item)
+        return item, 201
+
+
+class ItemsList(Resource):
+    def get(self):
+        return {"items": items}, 201
+
+
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemsList, "/items")
+
+app.run(port=5000, debug=True)
